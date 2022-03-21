82632745441c2848250284f27d159f833ff8bcb6
isadorinha <war25war2525@gmail.com>
2022-03-21 16:38:25 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### Como inciar o projeto
+Para iniciar o projeto, basta baixar o repositório e copiar o index.html para o navegador de sua preferência. 
---
+++ etapas.js
@@ -0,0 +1,53 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5, 
+        candidatos: [
+
+        {
+            numero:'38111', 
+            nome:'Fulano de Tal',
+            partido: 'ABC',
+            fotos:[
+                {url:'AndersHejlsberg.jpg', legenda: 'Vereador'}
+            ]
+        },
+        {
+            numero:'77222', 
+            nome: 'Beltrano da Silva', 
+            partido: 'DEFG', 
+            fotos: [
+                {url:'BrendamEich.jpg', legenda: 'Vereador'}
+            ]
+        },
+    ]
+
+    },
+    {
+        titulo:'PREFEITO', 
+        numeros: 2, 
+        candidatos: [
+         {
+            numero: '99', 
+            nome: 'Ciclano', 
+            partido: 'ABC', 
+            vice: 'Cic', 
+            fotos:[
+                {url:'GeraldALawson.jpg', legenda:'Prefeito'}, 
+                {url:'HadiyahNGreen.jpg', legenda: 'Vice-Prefeito', small: true}
+            ]
+        },
+        {
+            numero: '84', 
+            nome: 'Zulano', 
+            partido: 'QWERTY', 
+            vice: 'Zul', 
+            fotos:[
+                {url:'JaniceWilkins.jpg', legenda:'Prefeito'}, 
+                {url:'PhillipEmeagwali.jpg', legenda: 'Vice-Prefeito', small: true}
+            ]
+        },
+        ]
+    }
+]; 
+    
